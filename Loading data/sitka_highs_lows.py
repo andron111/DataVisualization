90d2dbb909cadb75947e5c52d5e4f664631f2bb9
andron111 <kli4ev2015@yandex.ru>
2021-11-26 21:35:11 +0300

    DataVisualization

--- Loading data/sitka_highs_lows.py
+++ Loading data/sitka_highs_lows.py
@@ -7,20 +7,24 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
     # Чтение дат и махсимальных температур из файла.
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], "%Y-%m-%d")
         high = int(row[5])
+        low = int(row[6])
         dates.append(current_date)
         highs.append(high)
+        lows.append(low)
 
 # Нанесение данных на диаграмму.
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(dates, highs, c='red')
+ax.plot(dates, highs, c='red', alpha=0.5)
+ax.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Форматирование диаграммы.
-plt.title("Daily high temperatures - 2018", fontsize=24)
+plt.title("Daily high and low temperatures - 2018", fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
