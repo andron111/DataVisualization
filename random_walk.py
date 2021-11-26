ac0e0cc2f28ee56730aac638284031583f518180
Web_1 <kli4ev2015@yandex.ru>
2021-11-26 08:27:08 +0300

    GeneratingData

--- random_walk.py
+++ random_walk.py
@@ -2,12 +2,36 @@ from random import choice
 
 
 class RandomWalk():
-    """Класс для генерирования случайных блужданиий."""
+    """Класс для генерирования случайных блужданий."""
 
     def __init__(self, num_points=5000):
-        """Инициализирует атрибуты блуждения."""
+        """Инициализирует атрибуты блуждания."""
         self.num_points = num_points
 
         # Все блуждания начинаются с точки (0, 0).
         self.x_values = [0]
         self.y_values = [0]
+
+    def fill_walk(self):
+        """Вычисляет все точки блуждания."""
+        while len(self.x_values) < self.num_points:
+
+            # Определение направления и длины перемещения.
+            x_direction = choice([1, -1])
+            x_distance = choice([0, 1, 2, 3, 4, 5, 6, 7, 8])
+            x_step = x_direction * x_distance
+
+            y_direction = choice([1, -1])
+            y_distance = choice([0, 1, 2, 3, 4, 5, 6, 7, 8])
+            y_step = y_direction * y_distance
+
+            # Отклонение нулeвых перемещений.
+            if x_step == 0 and y_step == 0:
+                continue
+
+            # Вычисление следующих значений x и y.
+            x = self.x_values[-1] + x_step
+            y = self.y_values[-1] + y_step
+
+            self.x_values.append(x)
+            self.y_values.append(y)
---
+++ rw_visual.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+from random_walk import RandomWalk
+
+while True:
+    # Построение случайного блуждания.
+    rw = RandomWalk(5000)
+    rw.fill_walk()
+
+    # Назначение точек на диаграмму.
+    plt.style.use('classic')
+    fig, ax = plt.subplots(figsize=(15, 9), dpi=128)
+    point_numbers = range(rw.num_points)
+    ax.plot(rw.x_values, rw.y_values, linewidth=1)
+    # ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
+
+    # Выделение первой и последней точек.
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    # Удаление осей.
+    # ax.get_xaxis().set_visible(False)
+    # ax.get_yaxis().set_visible(False)
+
+    plt.show()
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
