d17bca981aa0c5c63bd564b13865401684f63253
Web_1 <kli4ev2015@yandex.ru>
2021-11-26 08:49:57 +0300

    GeneratingData

--- mpl_squares.py
+++ mpl_squares.py
@@ -4,7 +4,7 @@ input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(input_values, squares, linewidth=3)
+ax.plot(input_values, squares, linewidth=2)
 
 # Назначение заголовка диаграммы и меток осей.
 ax.set_title("Square Numbers", fontsize=24)
--- scatter_squares.py
+++ scatter_squares.py
@@ -4,7 +4,7 @@ x_values = list(range(1, 1001))
 y_values = [x ** 2 for x in x_values]
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=9)
 
 # Назначение заголовка диаграммы и меток осей.
 ax.set_title("Square Numbers", fontsize=24)
