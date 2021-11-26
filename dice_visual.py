7d627fcd5eaa7e399f9093443602d2e727f353e3
Web_1 <kli4ev2015@yandex.ru>
2021-11-26 13:10:37 +0300

    Modeling of blocks of cubes in Plotly

--- dice_visual.py
+++ dice_visual.py
@@ -2,26 +2,28 @@ from die import Die
 from plotly.graph_objs import Bar, Layout
 from plotly import offline
 
-# Создание кубика D6.
-die = Die()
-
+# Создание кубика D6 и D10.
+die_1 = Die()
+die_2 = Die()
+die_3 = Die()
 # Моделирование серии бросков с сохранением результатов в списке.
 results = []
 for roll_num in range(1000):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll() + die_3.roll()
     results.append(result)
-
+print(results)
 # Анализ результатов.
 frequencies = []
-for value in range(1, die.num_sides+1):
+max_result = die_1.num_sides + die_2.num_sides + die_2.num_sides
+for value in range(3, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # Визуализация результатов.
-x_values = list(range(1, die.num_sides+1))
+x_values = list(range(3, max_result+1))
 data = [Bar(x=x_values, y=frequencies)]
 
-x_axis_config = {'title': 'Result'}
+x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title='Results of rolling one D6 1000 times', xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
+my_layout = Layout(title='Results of rolling two D6 and a D10 50000 times', xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d10.html')
